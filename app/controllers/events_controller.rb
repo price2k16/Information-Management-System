20a5d13ff9b3fedb6b5d4dfacd547559474d5309
price2k16 <pasemota2k@gmail.com>
2024-12-19 03:27:39 +0000

    more tweaks

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,43 +1,51 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    # Apply the appropriate scope based on the sort parameter in the URL
+    if params[:sort] == 'upcoming'
+      @events = Event.upcoming
+    elsif params[:sort] == 'past'
+      @events = Event.past
+    else
+      @events = Event.all
+    end
   end
 
   def show
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:id])  # Finding the event by its ID
   end
 
   def new
-    @event = Event.new
+    @event = Event.new  # Initialize a new event object
   end
 
   def create
     @event = Event.new(event_params)
 
-    if @event.save
+    if @event.save  # Save the new event and redirect if successful
       redirect_to @event
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity  # Render the new form again if saving fails
     end
   end
 
   def edit
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:id])  # Find the event by its ID for editing
   end
 
   def update
     @event = Event.find(params[:id])
-    if @event.update(event_params)
-      redirect_to @event
+
+    if @event.update(event_params)  # Update the event with new parameters
+      redirect_to @event  # Redirect to the updated event's page
     else
-      render :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity  # Show the edit form again if update fails
     end
   end
 
   def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to events_path
+    @event = Event.find(params[:id])  # Find the event to be deleted
+    @event.destroy  # Delete the event from the database
+    redirect_to events_path  # Redirect to the events index page after deletion
   end
 
   private
@@ -46,3 +54,4 @@ class EventsController < ApplicationController
     params.require(:event).permit(:title, :description, :start_time, :end_time, :guest_list)
   end
 end
+
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   validates :title, :start_time, :end_time, presence: true
+
+  # Scope to sort events by ascending start time (upcoming events)
+  scope :upcoming, -> { order(start_time: :asc) }
+
+  # Scope to sort events by descending start time (past events)
+  scope :past, -> { order(start_time: :desc) }
 end
