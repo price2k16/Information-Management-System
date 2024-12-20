da687ea18c7676d9997f4f719502457acfb3f71a
price2k16 <pasemota2k@gmail.com>
2024-12-20 18:13:20 +0000

    final tweaks

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   validates :title, :start_time, :end_time, presence: true
 
+  include PgSearch::Model
+pg_search_scope :search_by_title, against: :title
+
   # Scope to sort events by ascending start time (upcoming events)
   scope :upcoming, -> { order(start_time: :asc) }
 
