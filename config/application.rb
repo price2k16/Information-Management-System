4cd865c32d1d747bdace47b4f5aabe0c50ac7c4d
price2k16 <pasemota2k@gmail.com>
2024-12-20 02:31:57 +0000

    fixing bugs

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-# config/application.rb
-require "pg_search"
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
